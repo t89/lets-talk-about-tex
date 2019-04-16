628d07e843417703ac3d9102fc1d02de320d435b
Thomas Johannesmeyer <thomas@geeky.gent>
2019-04-16 17:26:36 +0200

    Implement latex compiling relatively to path

--- tex_compile.sh
+++ tex_compile.sh
@@ -9,10 +9,11 @@
 #
 
 
+cwd=$PWD
 # Recursively compile all .tex files within this repo
 for file in $(find . -type f -follow -name '*.tex')
-  do echo "Checking $file."
-    path=$(dirname "${file}")
+do echo "Checking $file."
+  path=$(dirname "${file}")
 
     # Clean Filename (no extension)
     filename=$(basename -- "$file")
@@ -24,31 +25,30 @@ for file in $(find . -type f -follow -name '*.tex')
     pdfname="${path_prefix/\//-}-$filename.pdf"
     pdf_path="$path/$filename.pdf"
 
-    if [ -f "$PWD/samples/$pdfname" ]; then
+    if [ -f "$cwd/samples/$pdfname" ]; then
       # Corresponding PDF already exists
       echo "$pdfname already exists. Skipping."
 
     else
       echo "Compiling $file."
 
-      pdflatex $file
+      cd $path
+      pdflatex "$filename.tex"
+
+      # Clean up
+      rm ./*.aux
+      rm ./*.log
+      rm ./*.out
 
       if [ -f "$filename.pdf" ]; then
         # Only attempt moving pdf, if it exists
-        mv "$filename.pdf" "$PWD/samples/$pdfname"
+        mv "$filename.pdf" "$cwd/samples/$pdfname"
 
       else
         echo "Attempted to move $filename.pdf. Non-existant. Check for LaTeX errors."
       fi
 
+      cd $cwd
     fi
 
-  done
-
-
-# Clean up
-rm ./*.aux
-rm ./*.log
-rm ./*.out
-
-echo "Cleaned directory"
+done
